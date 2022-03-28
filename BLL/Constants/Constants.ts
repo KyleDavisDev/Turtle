4428caa15fb9d45bb01c110d54e883c489986457
Kyle Davis <kyledavisdev@gmail.com>
2022-03-28 13:48:36 -0500

    small updates in order to implement interface

--- BLL/Constants/Constants.ts
+++ BLL/Constants/Constants.ts
@@ -1,8 +1,10 @@
+import { IConstant } from "./IConstant";
+
 const DEFAULT_WORD_LENGTH = 5;
 const DEFAULT_MAX_GUESSES = 6;
 const GAME_NAME = "Turtle";
 
-class Constants {
+class Constants implements IConstant {
   private _wordLength = DEFAULT_WORD_LENGTH;
   private _maxGuesses = DEFAULT_MAX_GUESSES;
   private _gameName = GAME_NAME;
@@ -11,19 +13,18 @@ class Constants {
     return this._wordLength;
   }
 
+  public set setWordLength(val: number) {
+    if (val < 1 || val > 9) {
+      throw new Error("The word size must be greater than 0 and less than 10.");
+    }
+    this._wordLength = val;
+  }
+
   public get maxGuesses() {
     return this._maxGuesses;
   }
 
-  public set wordLength(value: number) {
-    if (value < 1 || value > 9) {
-      throw new Error("The word size must be greater than 0 and less than 10.");
-    }
-
-    this._wordLength = value;
-  }
-
-  public set maxGuesses(guesses: number) {
+  public set setMaxGuesses(guesses: number) {
     if (guesses < 1 || guesses > 9) {
       throw new Error("The guess size must be greater than 0 and less than 10.");
     }
