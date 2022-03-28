8ed6fbb23bdc95ec126c46b0ec428afc2334603a
Kyle Davis <kyledavisdev@gmail.com>
2022-03-28 11:50:15 -0500

    Add constants class

--- BLL/Constants.ts
+++ BLL/Constants.ts
@@ -1,2 +1,40 @@
-export const DEFAULT_WORD_LENGTH = 5;
-export const DEFAULT_MAX_GUESSES = 6;
+const DEFAULT_WORD_LENGTH = 5;
+const DEFAULT_MAX_GUESSES = 6;
+const GAME_NAME = "Turtle";
+
+class Constants {
+
+  private _wordLength = DEFAULT_WORD_LENGTH;
+  private _maxGuesses = DEFAULT_MAX_GUESSES;
+  private _gameName = GAME_NAME;
+
+  public get wordLength(): number {
+    return this._wordLength;
+  }
+
+  public get maxGuesses() {
+    return this._maxGuesses;
+  }
+
+  public set wordLength(value: number) {
+    if (value < 0 || value > 10) {
+      throw new Error("The word size must be greater than 0 and less than 10.");
+    }
+
+    this._wordLength = value;
+  }
+
+  public set maxGuesses(guesses: number) {
+    if (guesses < 0 || guesses > 10) {
+      throw new Error("The guess size must be greater than 0 and less than 10.");
+    }
+
+    this._maxGuesses = guesses;
+  }
+
+  public get gameName() {
+    return this._gameName;
+  }
+}
+
+export { Constants };
