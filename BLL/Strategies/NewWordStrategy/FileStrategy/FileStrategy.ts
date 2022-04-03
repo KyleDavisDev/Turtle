8417aacd99fcc7056b96fda0cf575446908dbd03
Kyle Davis <kyledavisdev@gmail.com>
2022-04-03 16:44:47 -0500

    add interface for param, build out logic

--- BLL/Strategies/NewWordStrategy/FileStrategy/FileStrategy.ts
+++
@@ -1,9 +0,0 @@
-import { INewWordStrategy } from "../INewWordStrategy";
-
-class FileStrategy implements INewWordStrategy {
-  getWord(length: number): string {
-    return "";
-  }
-}
-
-export { FileStrategy };
---
+++ BLL/Strategies/NewWordStrategy/NewWordFileStrategy/NewWordFileStrategy.ts
@@ -0,0 +1,30 @@
+import { INewWordStrategy } from "../INewWordStrategy";
+import possibleWords from "./words.json";
+import { ISettings } from "../../../Settings/ISettings";
+
+export interface NewWordFileStrategyParams {
+  possibleWords?: string[];
+  settings: ISettings;
+}
+
+class NewWordFileStrategy implements INewWordStrategy {
+  private _possibleWords: string[];
+  private _settings: ISettings;
+
+  constructor(params: NewWordFileStrategyParams) {
+    this._possibleWords = params?.possibleWords ?? possibleWords;
+    this._settings = params.settings;
+  }
+
+  getWord(): string {
+    const wordsByLength = this._possibleWords.filter(word => word.length === this._settings.getWordLength);
+
+    if (!wordsByLength.length || wordsByLength.length === 0) {
+      throw new Error("Cannot find any words with that length");
+    }
+
+    return wordsByLength[Math.floor(Math.random() * wordsByLength.length)];
+  }
+}
+
+export { NewWordFileStrategy };
