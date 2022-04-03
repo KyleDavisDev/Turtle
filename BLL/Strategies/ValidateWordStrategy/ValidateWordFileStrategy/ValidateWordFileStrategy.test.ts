f58c135c06b6a94cbe2d1162a33d9fd5ad5e897a
Kyle Davis <kyledavisdev@gmail.com>
2022-04-03 17:02:21 -0500

    init commit

---
+++ BLL/Strategies/ValidateWordStrategy/ValidateWordFileStrategy/ValidateWordFileStrategy.test.ts
@@ -0,0 +1,54 @@
+import { ValidateWordFileStrategy, ValidateWordFileStrategyParams } from "./ValidateWordFileStrategy";
+
+describe("ValidateWordFileStrategy", () => {
+  const emptyList: string[] = [];
+  const possibleWordList = ["test", "cider", "apple", "hello", "world"];
+
+  it("should throw error when no word is passed", () => {
+    const params: ValidateWordFileStrategyParams = { possibleWords: emptyList };
+    const sut = new ValidateWordFileStrategy(params);
+
+    expect(() => {
+      sut.isValidWord(null);
+    }).toThrow();
+  });
+
+  it("should return false if word is not valid", () => {
+    // Given
+    const params: ValidateWordFileStrategyParams = { possibleWords: emptyList };
+    const sut = new ValidateWordFileStrategy(params);
+
+    // When
+    const isValid = sut.isValidWord("absafasdf");
+
+    // Then
+    expect(isValid).toBeFalsy();
+  });
+
+  it("should return true if the word is valid", () => {
+    // Given
+    const params: ValidateWordFileStrategyParams = { possibleWords: possibleWordList };
+    const sut = new ValidateWordFileStrategy(params);
+
+    // When
+    const isValid = sut.isValidWord(possibleWordList[0]);
+
+    // Then
+    expect(isValid).toBeTruthy();
+  });
+
+  it("should be case-insensitive", () => {
+    // Given
+    const params: ValidateWordFileStrategyParams = { possibleWords: possibleWordList };
+    const sut = new ValidateWordFileStrategy(params);
+
+    // When
+    let isValid = sut.isValidWord(possibleWordList[0].toUpperCase());
+
+    // Then
+    expect(isValid).toBeTruthy();
+
+    isValid = sut.isValidWord(possibleWordList[0].toLowerCase());
+    expect(isValid).toBeTruthy();
+  });
+});
