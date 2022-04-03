6eef5bf610c351913b140fbd51883522a921751c
Kyle Davis <kyledavisdev@gmail.com>
2022-04-03 13:58:14 -0500

    Update method signature style for clarity

--- BLL/Settings/ISettings.ts
+++ BLL/Settings/ISettings.ts
@@ -1,11 +1,11 @@
 export interface ISettings {
   getWordLength: number;
 
-  setWordLength(val: number);
+  setWordLength: (val: number) => void;
 
   getMaxGuesses: number;
 
-  setMaxGuesses(val: number);
+  setMaxGuesses: (val: number) => void;
 
   getGameName: string;
 }
