6e803cbe2865a3013cc74ad97da8dd8f7f720946
Kyle Davis <kyledavisdev@gmail.com>
2022-04-03 17:25:55 -0500

    cleanup

--- BLL/Strategies/ValidateWordStrategy/ValidateWordFileStrategy/ValidateWordFileStrategy.ts
+++ BLL/Strategies/ValidateWordStrategy/ValidateWordFileStrategy/ValidateWordFileStrategy.ts
@@ -1,17 +1,14 @@
 import { IValidateWordStrategy } from "../IValidateWordStrategy";
 
-// words from https://github.com/benjamincrom/scrabble/blob/master/scrabble/dictionary.json
-import scrabbleWords from "./scrabbleWords.json";
-
 export interface ValidateWordFileStrategyParams {
-  possibleWords?: string[];
+  possibleWords: string[];
 }
 
 class ValidateWordFileStrategy implements IValidateWordStrategy {
   private _words: Set<string>;
 
   constructor(params: ValidateWordFileStrategyParams) {
-    this._words = new Set<string>(params?.possibleWords ?? scrabbleWords);
+    this._words = new Set<string>(params.possibleWords);
   }
 
   isValidWord(word?: string): boolean {
