9e6ff2687e8ec8b79a59d101f39bc4f15eb5a536
Kyle Davis <kyledavisdev@gmail.com>
2022-04-03 19:10:53 -0500

    Bringing code in from BLL

--- Turtle/App.tsx
+++ Turtle/App.tsx
@@ -1,11 +1,17 @@
+import { GameFactory } from "../BLL/Game/GameFactory";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Header } from "./src/components/Header/Header";
+import { Game } from "./src/components/Game/Game";
 
 export default function App() {
+  const factory = new GameFactory();
+  const gameBLL = factory.badWordsMode();
+
   return (
     <View style={styles.container}>
       <Header />
+      <Game gameBLL={gameBLL} />
       <Text>Open up App.tsx to start working on your app!testasdf</Text>
       <StatusBar style="auto" />
     </View>
