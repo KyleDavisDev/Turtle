5ba24fe3854e089dba1a71f7f4210f4036e29eb1
Kyle Davis <kyledavisdev@gmail.com>
2022-04-09 17:44:48 -0500

    Render modals, what to do on win?

--- Turtle/App.tsx
+++ Turtle/App.tsx
@@ -3,16 +3,34 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Header } from "./src/components/Header/Header";
 import { Game } from "./src/components/Game/Game";
+import { ModalWinner } from "./src/components/Modals/ModalWinner/ModalWinner";
+import { useState } from "react";
+import { ModalInstructions } from "./src/components/Modals/ModalInstructions/ModalInstructions";
 
 export default function App() {
   const factory = new GameFactory();
-  const gameBLL = factory.badWordsMode();
+  const gameBLL = factory.standardGame();
+
+  const [isWinner, setIsWinner] = useState<boolean>(false);
+  const [pauseGame, setPauseGame] = useState<boolean>(false);
+
+  const onWinner = () => {
+    setIsWinner(true);
+    setPauseGame(true);
+
+    console.log("am i here?");
+  };
+
+  const onNewGameModeSelect = (mode: string): void => {
+    console.log(mode);
+  };
 
   return (
     <View style={styles.container}>
+      <ModalInstructions />
+      {isWinner && <ModalWinner onGameModeSelect={onNewGameModeSelect} />}
       <Header />
-      <Game gameBLL={gameBLL} />
-      <Text>Open up App.tsx to start working on your app!testasdf</Text>
+      <Game gameBLL={gameBLL} onWin={onWinner} />
       <StatusBar style="auto" />
     </View>
   );
@@ -21,7 +39,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#222",
     alignItems: "center",
     justifyContent: "center"
   }
