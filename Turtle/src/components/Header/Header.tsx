31c7acb24ae8b01b9faac40e20cfdbdb4fd65c8d
Kyle Davis <kyledavisdev@gmail.com>
2022-04-11 14:58:26 -0500

    Render icons

--- Turtle/src/components/Header/Header.tsx
+++ Turtle/src/components/Header/Header.tsx
@@ -1,23 +1,54 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { HelpIcon } from "./components/HelpIcon/HelpIcon";
+import { CogIcon } from "./components/CogIcon/CogIcon";
 
-const Header = () => {
+interface IHeader {
+  onInstructionsOpen: () => void;
+}
+
+const Header = (params: IHeader) => {
+  const { onInstructionsOpen } = params
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Turtle</Text>
+      <View style={styles.row}>
+        <View style={styles.section}>
+          <Pressable onPress={() => onInstructionsOpen()}>
+            <HelpIcon />
+          </Pressable>
+        </View>
+        <View style={styles.section}>
+          <Pressable>
+            <CogIcon />
+          </Pressable>
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    paddingVertical: 12,
-    alignItems: "center",
+    backgroundColor: "blue",
+    width: "100%",
+    borderBottomColor: "#222",
+    borderBottomWidth: 2,
+    marginBottom: 10,
+    display: "flex",
+    flexDirection: "row",
     justifyContent: "center"
   },
-  title: {
-    color: "#df928e",
-    fontWeight: "200",
-    fontSize: 32
+  row: {
+    width: "100%",
+    maxWidth: "1300px",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  section: {
+    display: "flex",
+    flexDirection: "row"
   }
 });
 
