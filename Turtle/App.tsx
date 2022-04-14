1f6fd5c8a50ea438937dbac6e79620587a6ea96f
Kyle Davis <kyledavisdev@gmail.com>
2022-04-14 09:38:16 -0500

    Move colors into file

--- Turtle/App.tsx
+++ Turtle/App.tsx
@@ -53,3 +53,10 @@ const styles = StyleSheet.create({
     // justifyContent: "center"
   }
 });
+
+export enum Colors {
+  "TRANSPARENT" = "rgba(0,0,0,1)",
+  "GREEN" = "rgb(39, 155, 78)",
+  "GRAY" = "rgb(67, 77, 93)",
+  "YELLOW" = "rgb(187, 145, 18)"
+}
