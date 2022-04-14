193b772ac5b67a881081231d549a5d2cbfa9c72c
Kyle Davis <kyledavisdev@gmail.com>
2022-04-14 10:12:24 -0500

    Renamed for clarity

---
+++ Turtle/src/Settings.ts
@@ -0,0 +1,11 @@
+export const CELL_ANIMATION_DURATION = 350;
+
+export enum Colors {
+  "TRANSPARENT" = "rgba(0,0,0,1)",
+  "GREEN" = "rgb(39, 155, 78)",
+  "GRAY" = "rgb(129, 131, 132)",
+  "DARKGREY" = "rgb(58, 58, 60)",
+  "YELLOW" = "rgb(187, 145, 18)",
+  "WHITE" = "rgb(245, 245, 245)",
+}
+
--- Turtle/src/Styles.ts
+++
@@ -1,6 +0,0 @@
-export enum Colors {
-  "TRANSPARENT" = "rgba(0,0,0,1)",
-  "GREEN" = "rgb(39, 155, 78)",
-  "GRAY" = "rgb(67, 77, 93)",
-  "YELLOW" = "rgb(187, 145, 18)"
-}
