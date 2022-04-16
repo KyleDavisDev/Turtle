fb5a7999abe954473113192af14c191468a764df
Kyle Davis <kyledavisdev@gmail.com>
2022-04-16 16:52:29 -0500

    Adjust guess amount

--- Turtle/src/components/Modals/ModalOptions/ModalOptions.tsx
+++ Turtle/src/components/Modals/ModalOptions/ModalOptions.tsx
@@ -6,12 +6,14 @@ export interface IModalProps {
   onGameModeSelect: (mode: string) => void;
   onLengthChange: (e: number) => void;
   wordLength: number;
+  onGuessLength: (e: number) => void;
+  guessLength: number;
   onClose: () => void;
   shouldDisplay: boolean | null;
 }
 
 const ModalOptions = (params: IModalProps) => {
-  const { onGameModeSelect, onClose, shouldDisplay, wordLength, onLengthChange } = params;
+  const { onGameModeSelect, onClose, shouldDisplay, wordLength, onLengthChange, guessLength, onGuessLength } = params;
 
   if (!shouldDisplay) return <></>;
 
@@ -29,52 +31,59 @@ const ModalOptions = (params: IModalProps) => {
 
         <View style={styles.sections}>
           <View style={styles.setting}>
-            <View>
+            <View style={styles.text}>
               <Text style={styles.sectionTitle}>Word Length</Text>
               <Text style={styles.sectionDescription}>Adjust how long the word is to guess.</Text>
             </View>
             <View style={styles.control}>
-              <Pressable onPress={() => onLengthChange(wordLength - 1)}><Text>-</Text></Pressable>
+              <Pressable style={[styles.bumper, styles.bumper_l]}
+                         onPress={() => onLengthChange(wordLength - 1)}><Text>-</Text></Pressable>
               <Text style={styles.input}>{wordLength}</Text>
-              <Pressable onPress={() => onLengthChange(wordLength + 1)}><Text>+</Text></Pressable>
+              <Pressable style={[styles.bumper, styles.bumper_r]}
+                         onPress={() => onLengthChange(wordLength + 1)}><Text>+</Text></Pressable>
             </View>
           </View>
           <View style={styles.setting}>
-            <View>
+            <View style={styles.text}>
               <Text style={styles.sectionTitle}>Guess Amount</Text>
               <Text style={styles.sectionDescription}>Adjust how many attempts you have to guess the word.</Text>
             </View>
             <View style={styles.control}>
-              <Text>sample</Text>
-            </View>
-          </View>
-
-          <View style={styles.options}>
-            <View style={styles.option}>
-              <Pressable style={styles.button} onPress={() => onGameModeSelect("Scrabble")}>
-                <Text>English Game</Text>
-              </Pressable>
-            </View>
-            <View style={styles.option}>
-              <Pressable style={styles.button} onPress={() => onGameModeSelect("Spanish")}>
-                <Text>Spanish Game</Text>
-              </Pressable>
-            </View>
-            <View style={styles.option}>
-              <Pressable style={styles.button} onPress={() => onGameModeSelect("Vanderbilt")}>
-                <Text>Vandy Game</Text>
-              </Pressable>
+              <Pressable style={[styles.bumper, styles.bumper_l]}
+                         onPress={() => onGuessLength(guessLength - 1)}><Text>-</Text></Pressable>
+              <Text style={styles.input}>{guessLength}</Text>
+              <Pressable style={[styles.bumper, styles.bumper_r]}
+                         onPress={() => onGuessLength(guessLength + 1)}><Text>+</Text></Pressable>
             </View>
           </View>
 
           <View style={styles.setting}>
+            <View style={styles.options}>
+              <View style={styles.option}>
+                <Pressable style={styles.button} onPress={() => onGameModeSelect("Scrabble")}>
+                  <Text>English Game</Text>
+                </Pressable>
+              </View>
+              <View style={styles.option}>
+                <Pressable style={styles.button} onPress={() => onGameModeSelect("Spanish")}>
+                  <Text>Spanish Game</Text>
+                </Pressable>
+              </View>
+              <View style={styles.option}>
+                <Pressable style={styles.button} onPress={() => onGameModeSelect("Vanderbilt")}>
+                  <Text>Vandy Game</Text>
+                </Pressable>
+              </View>
+            </View>
+          </View>
+
+          <View style={{ borderTopWidth: 1, borderTopColor: Colors.GRAY, paddingTop: 10 }}></View>
+          <View style={styles.setting}>
             <View>
-              <Text style={styles.sectionTitle}>Feedback</Text>
+              <Text style={styles.sectionTitle}>Feedback?</Text>
             </View>
             <View style={styles.control}>
-              <a
-                href="mailto:nytgames@nytimes.com?subject=Wordle%20Feedback&amp;body=%0D%0A%0D%0A%0A--%0ADevice%20summary%3A%0APage%3A%20%2Fgames%2Fwordle%0APlatform%3A%20Web%20(Desktop)%20%0ABrowser%3A%20Firefox%0AScreen%20Resolution%3A%201728%20x%201117%0AViewport%20Size%3A%201728%20x%20595%0ATimezone%3A%20UTC-5%0ABuild%3A%20e17c80f8%0A%20%20"
-                title="nytgames@nytimes.com">Email</a>
+              <Text>No Thanks</Text>
             </View>
           </View>
           <View style={styles.setting}>
@@ -152,6 +161,9 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     marginBottom: 15
   },
+  text: {
+    maxWidth: "66%"
+  },
   sectionTitle: {
     textTransform: "uppercase",
     color: Colors.WHITE,
@@ -161,7 +173,33 @@ const styles = StyleSheet.create({
     color: Colors.GRAY,
     fontSize: 14
   },
-  control: {},
+  control: { display: "flex", flexDirection: "row", alignItems: "stretch" },
+  bumper: {
+    backgroundColor: Colors.GRAY,
+    color: Colors.WHITE,
+    fontWeight: "700",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 5
+  },
+  bumper_l: {
+    borderTopLeftRadius: 5,
+    borderBottomLeftRadius: 5
+  },
+  bumper_r: {
+    borderTopRightRadius: 5,
+    borderBottomRightRadius: 5
+  },
+  input: {
+    borderWidth: 0,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    backgroundColor: Colors.WHITE,
+    color: Colors.BLACK
+  },
 
   options: {
     marginTop: "10px",
@@ -180,13 +218,8 @@ const styles = StyleSheet.create({
     paddingLeft: "15px",
     paddingRight: "15px",
     backgroundColor: "rgb(39, 155, 78)"
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10
   }
+
 });
 
 export { ModalOptions };
