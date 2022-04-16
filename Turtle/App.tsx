48bbfcb43db7b83d391e650c5753957ffc53ef49
Kyle Davis <kyledavisdev@gmail.com>
2022-04-16 16:24:30 -0500

    Adjust word length

--- Turtle/App.tsx
+++ Turtle/App.tsx
@@ -14,9 +14,10 @@ const factory = new GameFactory();
 const newGame = factory.spanishMode();
 export default function App() {
   const [game, setGame] = useState<IGame>(newGame);
+  const [wordLength, setWordLength] = useState<number>(newGame.getWordLength());
   const [isWinner, setIsWinner] = useState<boolean>(false);
   const [displayInstructions, setDisplayInstructions] = useState<boolean>(true);
-  const [displayOptions, setDisplayOptions] = useState<boolean | null>(null);
+  const [displayOptions, setDisplayOptions] = useState<boolean>(false);
   const [pauseGame, setPauseGame] = useState<boolean>(false);
   const [resetGame, setResetGame] = useState<boolean | null>(null);
 
@@ -59,17 +60,29 @@ export default function App() {
       }
     }
 
+    setWordLength(game.getWordLength());
+    setDisplayOptions(false);
+    setDisplayInstructions(false);
     setIsWinner(false);
     setPauseGame(false);
     setResetGame(!resetGame);
   };
 
+  const onLengthChange = (e: number): void => {
+    if (e < 1 || e > 9) return;
+
+    setWordLength(e);
+    factory.setWordLength(e);
+  };
+
   return (
     <View style={styles.container}>
-      <Header onInstructionsOpen={onInstructionsOpen} />
+      <Header onInstructionsOpen={onInstructionsOpen} onOptionsOpen={onOptionsOpen} />
       <ModalInstructions onClose={onInstructionsClose} shouldDisplay={displayInstructions} />
       {displayOptions &&
-        <ModalOptions onGameModeSelect={onNewGameModeSelect} onClose={onOptionsClose} shouldDisplay={displayOptions} />}
+        <ModalOptions onGameModeSelect={onNewGameModeSelect}
+                      wordLength={wordLength}
+                      onLengthChange={onLengthChange} onClose={onOptionsClose} shouldDisplay={displayOptions} />}
       {isWinner && <ModalWinner onGameModeSelect={onNewGameModeSelect} />}
       <Game gameBLL={game} onWin={onWinner} pauseGame={pauseGame} resetGame={resetGame} />
     </View>
